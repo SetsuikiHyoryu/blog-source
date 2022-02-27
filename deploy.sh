e72f531a83929901ebfb6ae6344e052bea95f26d
SETSUIKI HYORYU <tayutai@outlook.jp>
2022-02-27 17:28:11 +0900

    feat: update blog

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 生成静态文件
+npm run docs:build
+
 # 上传源码
 git add -A
 git commit -m 'feat: update blog'
 git push origin main
-
-# 生成静态文件
-npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
