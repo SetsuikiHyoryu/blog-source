9d68abce06b7e6190c92ad3404219b4df7b07d56
SETSUIKI HYORYU <tayutai@outlook.jp>
2022-11-05 04:33:45 +0900

    chore: update config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 import { useNavigation, useSidebar } from './composition'
 
 export default defineConfig({
-  base: '/next/',
+  base: '/blog-source/',
   lang: 'zh-CN',
   title: '冰龍與雪風的工作室',
   description: '冰龍與雪風的博客',
